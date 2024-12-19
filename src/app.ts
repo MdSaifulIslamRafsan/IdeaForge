9654b89adf0bb3d9b28ea8e7d806f0a3737f1c6c
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-20 04:29:39 +0600

    added handle unhandledRejection

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors';
 const app : Application = express();
 
-const port = process.env.PORT || 5000;
-
 app.use(express.json());
 app.use(cors());
 
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,37 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+import { Server } from 'http';
 
-const connectDB = async() => {
-    try { 
-        await mongoose.connect(config.db_url as string);
-        console.log("Database connected successfully!");  
+let server: Server;
 
-        app.listen(config.port, () => {
-            console.log(`Server is running on port ${config.port}`);
-          });
-          
-    } catch (error) {
-        console.error('Database connection failed:', error);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.db_url as string);
+    console.log('Database connected successfully!');
 
-connectDB(); 
+    server = app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.error('Database connection failed:', error);
+  }
+};
 
-process.on('uncaughtException', (error)=>{
-    console.error('🚨 An uncaught error occurred:', error);
-    process.exit(1);
-})
+connectDB();
+
+process.on('unhandledRejection', async (error) => {
+  console.error('🚨 Unhandled Rejection at:', error);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('🚨 An uncaught error occurred:', error);
+  process.exit(1);
+});
