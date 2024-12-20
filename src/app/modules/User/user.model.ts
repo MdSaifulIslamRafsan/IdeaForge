e1614c4232df41ced0fd580e694c5a37783bd274
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-21 05:03:38 +0600

    added user model

---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from 'mongoose';
+import { IUser } from './user.interface';
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = model<IUser>('User', userSchema)
+export default User;
