b4830f7de2686c9f6b9c31a73a6b12e4ba7a394c
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-21 23:47:50 +0600

    added updateBlogValidation

--- src/app/modules/Blog/blog.route.ts
+++ src/app/modules/Blog/blog.route.ts
@@ -6,5 +6,6 @@ import auth from '../../middleware/auth';
 const router = express.Router();
 
 router.post('/', auth() , BlogController.createBlog );
+router.patch('/:id', auth() , BlogController.createBlog );
 
 export const BlogRoutes =  router;
--- src/app/modules/Blog/blog.validation.ts
+++ src/app/modules/Blog/blog.validation.ts
@@ -11,7 +11,18 @@ const createBlogValidation = z.object({
     isPublished: z.boolean().optional(),
   }),
 });
+const updateBlogValidation = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }).optional(),
+    content: z.string({
+      required_error: 'Content is required',
+    }).optional(),
+  }),
+});
 
 export const BlogValidation = {
   createBlogValidation,
+  updateBlogValidation
 };
