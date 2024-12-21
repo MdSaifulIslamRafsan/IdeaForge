e10d11ed9c3f00be984ab3f68950f5b78aabfef9
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-21 19:21:15 +0600

    added validation request middelware

--- src/app/middleware/globalError.ts
+++ src/app/middleware/globalError.ts
@@ -1,19 +1,38 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import handleZodError from '../errors/handleZodError';
+import { ZodError } from 'zod';
+import { IErrorSources } from '../interface/error';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const globalErrorHandler :ErrorRequestHandler = (err, req : Request , res : Response , next : NextFunction) => {
-    const statusCode = err?.statusCode || 500;
-    const errorMessage = err?.message ||  'Validation error';
+const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
+) => {
+  let errorStatus = 500;
+  let errorMessage = 'An unexpected error occurred';
+  
+  let errorSource: IErrorSources[] = [
+    {
+      path: '',
+      message: 'An unexpected error occurred',
+    },
+  ];
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    errorStatus = simplifiedError.statusCode;
+    errorMessage = simplifiedError.message;
+    errorSource = simplifiedError.errorSources;
+  }
 
-    res.status(statusCode).send({
-        "success": false,
-        message : errorMessage,
-        statusCode,
-        error : err , 
-        stack: err?.stack,
-
-    })
-
+  res.status(errorStatus).send({
+    success: false,
+    message : errorSource[0]?.message || errorMessage,
+    errorStatus,
+    error: err,
+    stack: err?.stack,
+  });
 };
 
 export default globalErrorHandler;
